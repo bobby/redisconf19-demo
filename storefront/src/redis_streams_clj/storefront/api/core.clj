4fd68d7661541afc42dea681bc1c6dd975b32d94
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2019-04-03 15:51:15 -0700

    Give stable IDs to menu items

--- storefront/src/redis_streams_clj/storefront/api/core.clj
+++ storefront/src/redis_streams_clj/storefront/api/core.clj
@@ -35,17 +35,17 @@
         event))))
 
 (def menu-items
-  [{:id          (str (util/uuid))
+  [{:id          "e19fa2b0-5662-11e9-a902-dac8687b984b"
     :title       "Cappuccino"
     :description "Espresso with streamed milk. Classic."
     :price       1000
     :photo_url   "/img/products/cappuccino.jpg"}
-   {:id          (str (util/uuid))
+   {:id          "e7746720-5662-11e9-a902-dac8687b984b"
     :title       "Americano"
     :description "Espresso with hot water, like a 'Merican."
     :price       800
     :photo_url   "/img/products/americano.jpg"}
-   {:id          (str (util/uuid))
+   {:id          "ed3666e0-5662-11e9-a902-dac8687b984b"
     :title       "Hot Cocoa"
     :description "Cocoa powder, sugar, and streamed milk, for us non-coffee drinkers."
     :price       750
